1e163e7a17a203fa2798a26f45d3702268e7bfb4
Anna Maksimovich <hodosovskaaanna@gmail.com>
2025-11-06 07:50:01 +0300

    feat: do getStringsLength, getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -101,8 +101,8 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
 }
 
 /**
@@ -119,8 +119,13 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  const sum = arr.reduce((acc, cur) => acc + cur);
+  return Number((sum / arr.length).toFixed(2));
 }
 
 /**
